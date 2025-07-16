f8471eecfacd0a50260d9e75f28e644dbb28787d
Araz Omarov <05arazomarov@gmail.com>
2025-07-16 22:19:26 +0400

    12-classes_oop.py

---
+++ 12-classes_oop.py
@@ -0,0 +1,12 @@
+class Man:
+    def __init__(self, name, age):
+        self.name = "Araz"
+        self.age = 14
+    
+    def say_hello(self):
+        print(f"Hello, My Name is {self.name} and I'm {self.age} years old. ")
+        
+    
+
+a1 = Man("Araz", 14)
+a1.say_hello()

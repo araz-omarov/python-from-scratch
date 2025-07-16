af48eb4d5542c2215f99715ad0456e7efcad8bd5
Araz Omarov <05arazomarov@gmail.com>
2025-07-16 22:07:27 +0400

    1-Variables.py

---
+++ 1-Variables.py
@@ -0,0 +1,9 @@
+Name = "Araz Omarov"  # string
+Age = 14              # integer
+Height = 1.70         # float
+Muslim = True         # boolean
+
+print(type(Name))     # <class 'str'>
+print(type(Age))      # <class 'int'>
+print(type(Height))   # <class 'float'>
+print(type(Muslim))     # <class 'bool'>

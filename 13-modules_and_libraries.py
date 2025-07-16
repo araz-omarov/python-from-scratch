ceeda95fc3a2ca11b09d89c315704882be1fddf1
Araz Omarov <05arazomarov@gmail.com>
2025-07-16 22:19:40 +0400

    13-modules_and_libraries.py

---
+++ 13-modules_and_libraries.py
@@ -0,0 +1,13 @@
+from math import sqrt, pi
+print(sqrt(4))  # 4.0
+print(pi)        # 3.14159...
+
+#-------------------------
+
+#from math import sqrt, pi ---> yəni ki yalnız sqrt və pi daxil edilə bilər !
+# import ---> library
+# sqrt ---> √16 = 4.0
+# pi   ---> 3.141592653589793
+
+
+
